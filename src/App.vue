77a8686bda1c44172009a0be28e83c6bdd7d503e
cassandra645 <meursault45@live.com>
2020-09-22 16:20:56 +0200

    change general background according to temperature

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' :  ''">
     <main>
       <div class="search-box">
         <input type="text" class="search-bar" placeholder="Search a place..." v-model="query" @keypress="fetchWeather">
@@ -83,6 +83,10 @@ body {
   background-size: cover;
   background-position: bottom;
   transition: 0.4s;
+}
+
+#app.warm {
+  background-image: url('../src/assets/warm-bg.png');
 }
 
 main {
