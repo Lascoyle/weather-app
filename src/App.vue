2a3737048b27ed64780562dec5ddcc01c6af966c
cassandra645 <meursault45@live.com>
2020-09-22 16:12:00 +0200

    style modifications

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,14 @@
 
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">{{ weather.name }} {{ weather.sys.country }}</div>
           <div class="date">{{ dateBuilder() }}</div>
+          <div class="location">{{ weather.name }} {{ weather.sys.country }}</div>
         </div>
 
         <div class="weather-box">
           <div class="temp">{{ Math.round(weather.main.temp) }}°C</div>
           <div class="weather">{{ weather.weather[0].main }}</div>
+          <div class="weather-description">{{ weather.weather[0].description }}</div>
         </div>
       </div>
     </main>
@@ -107,22 +108,14 @@ main {
   background: none;
   box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
   background-color: rgba(255, 255, 255, 0.5);
-  border-radius: 0px 15px 0px 15px;
+  border-radius: 15px 15px 15px 15px;
   transition: 0.4s;
 }
 
 .search-box .search-bar:focus {
   box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
   background-color: rgba(255, 255, 255, 0.75);
-  border-radius: 15px 0px 15px 0px;
-}
-
-.location-box .location {
-  color: #ffffff;
-  font-size: 32px;
-  font-weight: 500;
-  text-align :center;
-  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+  border-radius: 15px 15px 15px 15px;
 }
 
 .location-box .date {
@@ -130,6 +123,14 @@ main {
   font-size: 20px;
   font-weight: 300;
   font-style: italic;
+  text-align :center;
+  margin-bottom: 70px;
+}
+
+.location-box .location {
+  color: #ffffff;
+  font-size: 50px;
+  font-weight: 500;
   text-align :center;
 }
 
@@ -141,22 +142,24 @@ main {
   display: inline-block;
   padding: 10px 25px;
   color: #ffffff;
-  font-size: 102px;
+  font-size: 80px;
   font-weight: 900;
   text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
   background-color: rgba(255, 255, 255, 0.25);
-  border-radius: 16px;
+  border-radius: 15px;
   margin: 30px 0px;
-
-  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
 }
 
 .weather-box .weather{
   color: #ffffff;
-  font-size: 48px;
+  font-size: 30px;
   font-weight: 700;
   font-style: italic;
-  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .weather-description{
+  color: #ffffff;
+  font-style: italic;
 }
 
 </style>
